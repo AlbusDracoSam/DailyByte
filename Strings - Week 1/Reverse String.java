3867e91b36835b3c15994509bc566103301bb583
Ajith Samraj <61262685+AlbusDracoSam@users.noreply.github.com>
2021-07-06 21:03:30 +0530

    Added Reverse String.java

---
+++ Strings - Week 1/Reverse String.java
@@ -0,0 +1,20 @@
+//Leetcode : [Reverse String](https://leetcode.com/problems/reverse-string/)
+
+
+
+class Solution {
+    public void reverseString(char[] s) {
+        
+        int start = 0 ; 
+        int end = s.length-1 ;
+        
+        while(start < end)
+        {
+            char temp = s[start];
+            s[start] = s[end];
+            s[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
