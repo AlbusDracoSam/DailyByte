d17bab7cd017eb790c464b50edfda7398812e0a4
Ajith Samraj <61262685+AlbusDracoSam@users.noreply.github.com>
2021-07-18 22:09:17 +0530

    Added 1. Merge Linked Lists.java

---
+++ Week - 3 Linked Lists/1. Merge Linked Lists.java
@@ -0,0 +1,37 @@
+// https://leetcode.com/problems/merge-two-sorted-lists/
+
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        
+        ListNode dummy = new ListNode(-1);
+        ListNode head = dummy;
+        
+        while(l1 != null && l2 != null)
+        {
+            
+            if(l1.val < l2.val)
+            {
+                dummy.next = l1;
+                l1 = l1.next;
+            }
+            else{
+                dummy.next = l2;
+                l2 = l2.next; 
+            }
+            
+            dummy = dummy.next;
+            
+        }
+        
+        if(l1 != null)
+        {
+            dummy.next = l1;
+        }
+        else{
+            dummy.next = l2;
+        }
+        
+        return head.next;
+        
+    }
+}
