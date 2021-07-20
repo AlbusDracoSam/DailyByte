b625a9d5528c649a9dbbf7401d815fd086af0d96
Ajith Samraj <61262685+AlbusDracoSam@users.noreply.github.com>
2021-07-20 21:09:02 +0530

    Added 3. Remove Linked List Elements.java

---
+++ Week - 3 Linked Lists/3. Remove Linked List Elements.java
@@ -0,0 +1,23 @@
+//https://leetcode.com/problems/remove-linked-list-elements/
+
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        
+        while(head != null && head.val == val)
+            head = head.next;
+        
+        ListNode cur = head;
+        
+        while(cur != null && cur.next != null)
+        {
+            if(cur.next.val == val)
+            {
+                cur.next = cur.next.next;
+            }
+            else{
+                cur = cur.next;
+            }
+        }
+        return head;
+    }
+}
