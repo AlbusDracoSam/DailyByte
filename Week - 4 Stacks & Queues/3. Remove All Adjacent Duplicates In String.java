e7d6e64b271495c69154a701b4fb96afa43bf456
Ajith Samraj <61262685+AlbusDracoSam@users.noreply.github.com>
2021-07-28 23:48:43 +0530

    Added 4. Remove All Adjacent Duplicates In String.java

---
+++ Week - 4 Stacks & Queues/3. Remove All Adjacent Duplicates In String.java
@@ -0,0 +1,26 @@
+//https://leetcode.com/problems/remove-all-adjacent-duplicates-in-string/
+
+class Solution {
+    public String removeDuplicates(String s) {
+        
+        Stack<Character> stack = new Stack<>();
+        
+        for(char c : s.toCharArray())
+        {
+            if(!stack.isEmpty() && stack.peek() == c)
+                stack.pop();
+            else
+                stack.push(c);
+        }
+        
+        String res = "";
+        
+        while(!stack.isEmpty())
+        {
+            res = stack.pop() + res;
+        }
+        
+        return res;
+        
+    }
+}
